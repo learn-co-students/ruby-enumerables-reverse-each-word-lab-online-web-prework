f3972d8de66b97e8b84e055b8de7c9ee692fc58c
Haim D <haim11293@aol.com>
2019-06-18 20:15:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word_using_each(string)
+  new_array = string.split(" ")
+  return_array = []
+  new_array.each do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  return_array = []
+  new_array.collect do |string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
