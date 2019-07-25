f12ae4b15cdf17e683ed813f9f4d555143da9fb8
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-25 04:14:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,3 @@
+(reverse_each_word(sentence1)) .to eq( "olloeH ,ereht dna woh era ?uoy")
+(reverse_each_word(sentence2)) .to eq( "iH , niaga tsuj g nikam erus a'ti !desrever")
+reverse_each_word("verifying that collect is being called.")
