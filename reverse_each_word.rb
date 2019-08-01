aa95157fe61b0744ce28db3f6ee4fe5ec0117b51
Esther Agyare-Boateng <eagyare91@gmail.com>
2019-08-01 21:46:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(array)
+  array = string.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse 
+    
+    end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
