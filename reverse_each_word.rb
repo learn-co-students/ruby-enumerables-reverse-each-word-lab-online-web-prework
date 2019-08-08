7fbb0611746fd3e1cc704eb65b00b40c4ce9ffdc
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 19:41:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+
+def '#reverse_each_word'
+when 'reverses all the words in another string without reversing the order of the words' do.
+end
+  {:sentence1 = "Hello there, and how are you?"
+  },
+  {:sentence2 = "Hi again, just making sure it's reversed!"
+
+  },
+
+end
+
+'#reverse_each_word'.each
+'#reverse_each_word'.collect
