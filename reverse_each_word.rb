2909290f432b97c982f4b3af00e04153c878c3ce
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-09 08:21:59 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence1)
+  "Hello there, and how are you".split(",")
+  array=[reverse_each_word]
+  reverse_each_word.each{|w| w.reverse}
+end
+
+def reverse_each_word(sentence2)
+  "Hello there, and how a"
