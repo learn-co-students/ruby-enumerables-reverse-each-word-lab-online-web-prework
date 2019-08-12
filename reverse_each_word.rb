4b297d7ffacb37957b38c4a5cee5f88076fb78e1
github username <github email address>
2019-08-12 17:07:01 -0400

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+def reverse_each_word(characters)
+  return (characters.split.collect do |some_characters| some_characters.reverse end).join(" ");
+end
+
+
+p reverse_each_word("Hello there, and how are you?")
