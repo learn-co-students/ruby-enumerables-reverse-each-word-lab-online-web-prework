992211ea8a59a42f0886d85b7bd24260abd084ac
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-12 11:36:26 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,12 @@
-def reverse_each_word("Hello there, and how are you?")
-  string = string.split(" ")
-  reversed_string = []
-  string.each do |char|
-    reversed_string.unshift(char)
-    end
-    return reversed_string.join(" ")
-  end
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
 end
 
