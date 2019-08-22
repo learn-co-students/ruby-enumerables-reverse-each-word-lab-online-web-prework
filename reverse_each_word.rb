a4e06cdc831e2d59ce4d2727980f94e2b28a4fa3
Shamel Lakin <christlife4u@gmail.com>
2019-08-22 16:05:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word = ["Hello there, and how are you?"]
+  reverse_each_word.each do |reverse_each_word|
+     puts #{reverse_each_word.reverse} 
+end
+
+def reverse_each_word = ["Hello there, and how are you?"]
+  reverse_each_word.collect do |reverse_each_word|
+    puts #{reverse_each_word.collect}
+end
