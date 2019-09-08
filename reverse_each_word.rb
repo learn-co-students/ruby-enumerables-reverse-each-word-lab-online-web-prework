5da4df373fac8aef0d93147fd37edde36645b3f9
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-08 13:11:08 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+#def reverse_each_word(string)
+#  array = string.split
+#  new_array = []
+#  
+#  array.each do |element| 
+#    new_array.push(element.reverse)
+#  end
+#  new_array.join(" ")
+#end
+
+def reverse_each_word(string)
+  array = string.split
+  
+  array.collect do |element| 
+    element.reverse
+  end.join(" ")
+end
+
