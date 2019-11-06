3dc1785092b5503ec2bcba797824c319f731adee
Carlos Barreiro <bcarlosro@gmail.com>
2019-11-06 18:39:53 +0000

    Done.

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -1,3 +1,12 @@
+def reverse_each_word(string)
+  array_of_words = string.split(" ")
+    backwards_array = array_of_words.collect do |word|
+      word = word.reverse
+    end
+  string2 = backwards_array.join(" ")
+  string2
+end
+
 require 'spec_helper'
 
 describe '#reverse_each_word' do
