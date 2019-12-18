39f2ef7fdd7762f293dd96f1d1c74caf8b55596f
Dan Foley <dan@dan-foley.com>
2019-12-18 00:45:32 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,11 @@
-%w(Hello there, and how are you?).each 
+def reverse_each_word(string)
+  arr = string.split(/ /)
+  result = ""
+  arr.collect do |word|
+     result += word.reverse + " "
+    # result
+  end
+  result.strip
+end
+
+reverse_each_word("Hello there, and how are you?")
