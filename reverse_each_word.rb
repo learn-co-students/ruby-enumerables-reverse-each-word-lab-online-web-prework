c48f5c2ce7959df75dee00e7aba1eea3a4041efc
fadimata tillman <fadi.tillman@gmail.com>
2020-01-20 02:28:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+reverse_each_word = (Hello there, and how are you?") 
+reverse_each_word.each do |element1,element2|
+   puts "#{element1,element2.reverse.}
+   
+   reverse_each_word = (Hello there, and how are you?") 
+reverse_each_word.collect do |element1,element2|
+   puts "#{element1,element2.reverse.}
