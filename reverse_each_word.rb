75ebe66798b754303bfbe4b2b42634eba73d0d09
Ebaa Abdelfadeel <ebaa.abdelfadeel@gmail.com>
2020-05-12 22:11:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,4 @@
-require 'pry'
-
 def reverse_each_word(sentence)
   array = sentence.split(/ /)
-  reversed = []
-  array.collect do |word| 
-    reversed << word.reverse
-  end
-  reversed.join(" ")
+  array.collect{|word| word.reverse}.join(" ")
 end
-    
- 
-
