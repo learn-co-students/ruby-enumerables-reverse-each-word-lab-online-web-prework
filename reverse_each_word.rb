0285aa2c7fdd1300e1c777692d32cb62bf0ef22e
Ian King <kickthechair@gmail.com>
2021-05-03 14:47:00 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,11 @@ def reverse_each_word(string)
   reversed_array = into_array.collect { |element| element.reverse }
   reversed_array.join(" ")
 end
+
+
+
+#This method can be further refined to 'chain' methods together. This creates cleaner, succinct and better understandable code
+
+def reverse_each_word(string)
+  string.split.collect { |element| element.reverse }.join(" ")
+end
